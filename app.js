f6f03f37936dd3cfb5f9904c2e410b07f79cb5c2
solyaridz3 <solyaridz3@gmail.com>
2022-07-09 00:37:49 +0300

    swagger docs added

--- app.js
+++ app.js
@@ -14,22 +14,31 @@ const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 const authMiddleware = require('./middleware/authentication');
 
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 app.set('trust proxy', 1)
-app.use(rateLimiter({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
-}));
+
 app.use(express.json())
 
 app.use(helmet());
 app.use(cors());
 app.use(xss());
+app.use(rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+}));
 
 // routes
+
 app.get('/', (req, res) => {
-    res.send('<h1>Store api</h1><a href="api/v1/products">products route</a>')
-})
+    res.send('<h1>Store api</h1><a href="/api-docs">API Documentation</a>')
+});
+
+app.use('/api-docs', [swaggerUI.serve, swaggerUI.setup(swaggerDocument)]);
+
+
 
 app.use('/api/v1/products', [authMiddleware, productsRouter]);
 app.use('/api/v1/auth', authRouter);
